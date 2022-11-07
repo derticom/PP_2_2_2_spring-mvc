112d84489f82fc60780132e1e572209c58f90875
Maxim Egorov <max-egorov@inbox.ru>
2022-11-07 21:36:23 +0300

    Add DAO layer

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,29 +1,23 @@
 package web.controller;
 
-import model.Car;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import service.CarService;
-import service.CarServiceImpl;
-
-import java.util.ArrayList;
-import java.util.List;
+import web.service.CarServiceImpl;
 
 @Controller
 public class CarController {
 
-    CarService carService = new CarServiceImpl();
+    @Autowired
+    @Qualifier("carServiceImpl")
+    private CarServiceImpl carServiceImpl;
 
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(defaultValue = "5") int count, ModelMap model) {
-        List<Car> list = new ArrayList<>();
-        if (count > 5) count = 5;
-        for (int i = 0; i < count; i++) {
-            list.add(carService.getCar().get(i));
-        }
-        model.addAttribute("car", list);
+        model.addAttribute("car", carServiceImpl.getCar(count));
         return "/cars";
     }
 
---
+++ src/main/java/web/dao/CarDao.java
@@ -0,0 +1,7 @@
+package web.dao;
+
+import web.model.Car;
+
+public interface CarDao {
+    void addCar(Car car);
+}
