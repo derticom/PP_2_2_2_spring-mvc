374b02588510c002aabea6b5222513f70d65ac63
Maxim Egorov <max-egorov@inbox.ru>
2022-11-10 17:39:30 +0300

    CarService carService

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,20 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarServiceImpl;
+import web.service.CarService;
 
 @Controller
 public class CarController {
 
-    private final CarServiceImpl carServiceImpl;
+    private final CarService carService;
 
-    public CarController(CarServiceImpl carServiceImpl) {
-        this.carServiceImpl = carServiceImpl;
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
 
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(defaultValue = "5") int count, ModelMap model) {
-        model.addAttribute("car", carServiceImpl.getCar(count));
+        model.addAttribute("car", carService.getCar(count));
         return "/cars";
     }
 
